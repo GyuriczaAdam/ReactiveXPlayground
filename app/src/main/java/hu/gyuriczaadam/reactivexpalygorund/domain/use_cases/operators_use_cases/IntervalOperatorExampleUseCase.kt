d5ba7c4452c5a93a2ef0c3807ad3d88a1d49721d
Gyuricza Ádám <gyuricza.adam@fizetesipont.hu>
2022-09-23 10:45:56 +0200

    callable operator example

--- app/src/main/java/hu/gyuriczaadam/reactivexpalygorund/domain/use_cases/operators_use_cases/IntervalOperatorExampleUseCase.kt
+++ app/src/main/java/hu/gyuriczaadam/reactivexpalygorund/domain/use_cases/operators_use_cases/IntervalOperatorExampleUseCase.kt
@@ -1,5 +1,6 @@
 package hu.gyuriczaadam.reactivexpalygorund.domain.use_cases.operators_use_cases
 
+import hu.gyuriczaadam.reactivexpalygorund.data.operators_example.Task
 import hu.gyuriczaadam.reactivexpalygorund.di.ViewModelScope
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -12,10 +13,16 @@ import javax.inject.Singleton
 class IntervalOperatorExampleUseCase {
     operator fun invoke(): Observable<Long> {
         //Emit an observable every time interval
+
         return Observable
+            //Emits observables from array
+            //.fromArray(array)
+            //Emits observable from an iterable(like a list)
+            //.fromIterable(list)
             .timer(1,TimeUnit.SECONDS)
             .subscribeOn(io())
             .observeOn(AndroidSchedulers.mainThread())
+
             //Emit an observable every time interval
             //.interval(1,TimeUnit.SECONDS)
             //Emit a single observable after a given delay
